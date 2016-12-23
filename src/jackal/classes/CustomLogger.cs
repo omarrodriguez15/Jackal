7ba227d16e96027f5505359570648da817231fa2
omarrodriguez15 <rodriguezomar91@gmail.com>
2016-12-22 22:23:14 -0600

    Added a flag to logger to suppress console output -Added two simple test cases to unit test

--- src/jackal/classes/CustomLogger.cs
+++ src/jackal/classes/CustomLogger.cs
@@ -6,9 +6,18 @@ namespace Jackal
    public class CustomLogger 
    {
       private static StringBuilder _sb = new StringBuilder();
+      private static bool _consoleOut = true;
+
+      public static bool ConsoleOut 
+      {
+         set{ _consoleOut = value;}
+         get{ return _consoleOut;}
+      }
+
       public static void Log(string msg, bool consoleOut = true, bool tofile = false)
       {
-         Console.Out.WriteLine(msg);
+         if(_consoleOut)
+            Console.Out.WriteLine(msg);
          _sb.AppendLine(msg);
       }
       public static void Log(Player player)
---
+++ test/jackal.tests/test files/SimulatorTesting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using Jackal;
+namespace Tests
+{
+    public class SimulatorTesting
+    {
+        private int _numOfDecks = 4, _numOfPlayers = 2, _baseBet = 10, _betAmt = 10, _numOfRounds = 10;
+
+        public SimulatorTesting()
+        {
+            CustomLogger.ConsoleOut = false;
+        }
+
+        [Fact]
+        public void SimulatorShouldInitializeProperly() 
+        {
+            BlackJackSim blackJack = new BlackJackSim(_numOfDecks, _numOfPlayers, _baseBet, _betAmt);
+            blackJack.SimulateRounds(_numOfRounds);
+            Assert.NotNull(blackJack.mainDeck);
+        }
+
+        [Fact]
+        public void CardDeckShouldBeFull() 
+        {
+            BlackJackSim blackJack = new BlackJackSim(_numOfDecks, _numOfPlayers);
+            Assert.Equal(blackJack.mainDeck.cards.Count, _numOfDecks * 52);
+        }
+    }
+}
