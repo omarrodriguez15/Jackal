6b7244c2055178d5875d45f4d85b83475bffd318
omarrodriguez15 <rodriguezomar91@gmail.com>
2016-12-22 21:56:06 -0600

    Re arranged to add unit test

---
+++ test/jackal.tests/test files/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Jackal;
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+            BlackJackSim blackJack = new BlackJackSim(1, 2);
+            blackJack.SimulateRounds(3);
+            Assert.NotNull(blackJack.mainDeck);
+        }
+    }
+}
